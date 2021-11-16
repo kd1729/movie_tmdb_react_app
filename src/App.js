d2d017a2da21341307ca63137d19a60418782270
onlykingKD <kaustubhdwivedi1729@gmail.com>
2021-11-17 00:58:57 +0530

    Added API Key

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 // https://www.themoviedb.org/documentation/api
-
+import React, {useState, useEffect} from "react";
 
 import Header from "./Components/Header";
 function App() {
-  
 
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    setMovies([]);
+  }, [])
 
   return (
     <div className="App">
