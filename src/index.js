1be4f3d218e1bc3df317cadc9592c9d160960cdc
onlykingKD <kaustubhdwivedi1729@gmail.com>
2021-11-20 21:15:33 +0530

    Created Route Components

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Favourites from './Components/Favourites';
+import WatchLater from './Components/WatchLater';
+import Completed from './Components/Completed';
 
 ReactDOM.render(
-    <App />,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}/>
+      <Route path="/favourites" element={<Favourites />}/>
+      <Route path="/WatchLater" element={<WatchLater />}/>
+      <Route path="/Completed" element={<Completed />}/>
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
+
 
